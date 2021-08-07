a55276a04ffb23c78252df3310b4bc358babdbb2
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-07 14:16:13 -0300

    class1

---
+++ class1/main.js
@@ -0,0 +1,9 @@
+var phrase = "Japan is the best team the world";
+
+console.log(phrase);
+console.log(phrase.replace("Japan", "SPFC")); // replace change the elements
+
+alert(phrase);
+alert(phrase.replace("Japan", "SPFC"));
+
+console.log(phrase.toLowerCase()); 
